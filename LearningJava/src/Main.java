a8e304031b1711ace4fd1367b23e4656573131bd
Richard Azia <richard@main-vision.com>
2022-10-01 17:31:45 +0200

    primitive data types

--- LearningJava/src/Main.java
+++ LearningJava/src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int studentAge = 40;
+        double studentGPA = 3.14;
+        char studentFirstInitial = 'R';
+        char studentLastInitial = 'A';
+        boolean neverSick = true;
+
+        System.out.println(studentAge);
+        System.out.println(studentGPA);
+        System.out.println(studentFirstInitial);
+        System.out.println(studentLastInitial);
+        System.out.println(neverSick);
     }
 }
