f6a39f92b5013f9d65bd24d3b0835aef0fe120af
Richard Azia <richard@main-vision.com>
2022-10-01 16:47:10 +0200

    Initial commit

---
+++ hello_world.java
@@ -0,0 +1,7 @@
+public class hello_world {
+
+  public static void main(String[] args) {
+      System.out.println("Hello, written in VIM");
+      System.out.println("Hello, I am line 2, not to be confused with song 2");
+  }
+}
